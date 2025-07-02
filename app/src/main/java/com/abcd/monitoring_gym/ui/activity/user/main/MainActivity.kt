7023df55a6cf09646f48652f9d38cf67cd190df0
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-07-02 14:38:54 +0800

    move main activity

--- app/src/main/java/com/abcd/monitoring_gym/ui/activity/user/main/MainActivity.kt
+++ app/src/main/java/com/abcd/monitoring_gym/ui/activity/user/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.abcd.monitoring_gym
+package com.abcd.monitoring_gym.ui.activity.user.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.abcd.monitoring_gym.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
