79aaf3b9d99e87c3058e7d2f7c503d0a33701147
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-07-05 00:37:55 +0800

    add parcelable

--- app/src/main/java/com/abcd/monitoring_gym/data/model/UserModel.kt
+++ app/src/main/java/com/abcd/monitoring_gym/data/model/UserModel.kt
@@ -1,5 +1,7 @@
 package com.abcd.monitoring_gym.data.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 class UserModel (
@@ -26,4 +28,41 @@ class UserModel (
 
     @SerializedName("sebagai")
     var sebagai: String? = null,
-)
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(idUser)
+        parcel.writeString(nama)
+        parcel.writeString(nomor_hp)
+        parcel.writeString(alamat)
+        parcel.writeString(jenis_kelamin)
+        parcel.writeString(username)
+        parcel.writeString(password)
+        parcel.writeString(sebagai)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<UserModel> {
+        override fun createFromParcel(parcel: Parcel): UserModel {
+            return UserModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<UserModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
