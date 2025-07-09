7af44345fb0f8d6fef14412b4797c3db8d2423b8
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-07-09 14:41:06 +0800

    add existing order or agenda data

--- app/src/main/java/com/abcd/monitoring_gym/ui/fragment/user/agenda/AgendaFragment.kt
+++ app/src/main/java/com/abcd/monitoring_gym/ui/fragment/user/agenda/AgendaFragment.kt
@@ -6,15 +6,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.abcd.monitoring_gym.R
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.abcd.monitoring_gym.adapter.PesananAdapter
+import com.abcd.monitoring_gym.data.model.PesananModel
 import com.abcd.monitoring_gym.databinding.FragmentAgendaBinding
+import com.abcd.monitoring_gym.utils.SharedPreferencesLogin
+import com.abcd.monitoring_gym.utils.network.UIState
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.ArrayList
 
+@AndroidEntryPoint
 class AgendaFragment : Fragment() {
     private lateinit var binding: FragmentAgendaBinding
+    private lateinit var sharedPreferences: SharedPreferencesLogin
     private val viewModel: AgendaViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        sharedPreferences = SharedPreferencesLogin(requireContext())
+        viewModel.fetchPesanan(sharedPreferences.getIdUser())
     }
 
     override fun onCreateView(
@@ -28,6 +38,55 @@ class AgendaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        getPesanan()
     }
+
+    private fun getPesanan() {
+        viewModel.getPesanan.observe(viewLifecycleOwner){result->
+            when(result){
+                is UIState.Loading-> setStartShimmer()
+                is UIState.Failure-> setFailurePesanan(result.message)
+                is UIState.Success-> setSuccessPesanan(result.data)
+            }
+        }
+    }
+
+    private fun setFailurePesanan(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+        setStopShimmer()
+    }
+
+    private fun setSuccessPesanan(data: ArrayList<PesananModel>) {
+        if(data.isNotEmpty()){
+            setAdapterPesanan(data)
+        } else{
+            Toast.makeText(requireContext(), "Tidak ada data", Toast.LENGTH_SHORT).show()
+        }
+        setStopShimmer()
+    }
+
+    private fun setAdapterPesanan(data: ArrayList<PesananModel>) {
+        val adapterPesanan = PesananAdapter(data)
+        binding.rvPesanan.apply {
+            adapter = adapterPesanan
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+    }
+
+    private fun setStartShimmer(){
+        binding.apply {
+            smPesanan.startShimmer()
+            smPesanan.visibility = View.VISIBLE
+            rvPesanan.visibility = View.GONE
+        }
+    }
+
+    private fun setStopShimmer(){
+        binding.apply {
+            smPesanan.stopShimmer()
+            smPesanan.visibility = View.GONE
+            rvPesanan.visibility = View.VISIBLE
+        }
+    }
+
 }
